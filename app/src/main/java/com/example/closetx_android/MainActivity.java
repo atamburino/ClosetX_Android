f6dbc6667fa603beaf7635a6b8e23336eee9a35c
Andy <57277787+atamburino@users.noreply.github.com>
2021-09-08 22:35:34 -0400

    Creat button to travel between 2 activities

--- app/src/main/java/com/example/closetx_android/MainActivity.java
+++ app/src/main/java/com/example/closetx_android/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.closetx_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button move;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+       //move button logic
         setContentView(R.layout.activity_main);
+        move=findViewById(R.id.Move);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,second.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+
+
+
 }
